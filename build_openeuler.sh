51e1b4f78492268ee1f2f29210013ce25c0eb3e9
Fan Lan <lanfanllrr@gmail.com>
2022-10-24 16:06:33 +0800

    add python 3.9

---
+++ build_openeuler.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for d in openeuler
+do
+	echo "====> $d"
+	podman build --security-opt seccomp=unconfined --rm --no-cache -v /data:/data -v /root:/root \
+		-t docker.io/lanfanb/siqbase:tools.$d -f Dockerfile.$d .
+	podman rmi docker.io/lanfanb/siqbase:tools.$d
+done
+
