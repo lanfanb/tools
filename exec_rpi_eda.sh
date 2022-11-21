03d3f62611e858f009f5bde9b97414fb4f8cdb73
Fan Lan <lanfanllrr@gmail.com>
2022-11-21 15:59:52 +0800

    add or-tools on rpi

--- exec_rpi_eda.sh
+++ exec_rpi_eda.sh
@@ -5,15 +5,30 @@ mkdir -p /data/tools
 export TZ="Asia/Shanghai"
 export d=$(date +'%Y.%m.%d')
 
+# or-tools
+cd /data/src/or-tools
+tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
+tag=${tag//v/}
+tag=${tag//\^*/}
+if [[ "$tag" == "undefined" ]]; then tag=$d; fi
+rm -rf /data/tools/or-tools-$tag
+cmake -S. -Bbuild -DBUILD_DEPS:BOOL=ON \
+	-DCMAKE_INSTALL_PREFIX=/data/tools/or-tools-$tag -DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache
+cmake --build build
+cmake --build build --target install
+export INSTALL_ORTOOLS=/data/tools/or-tools-$tag
+
 # OpenROAD
 cd /data/src/OpenROAD
 tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 tag=${tag//v/}
-tag=${tag//^*/}
+tag=${tag//\^*/}
 if [[ "$tag" == "undefined" ]]; then tag=$d; fi
 rm -rf /data/tools/OpenROAD-$tag
 mkdir -p build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/data/tools/OpenROAD-$tag -DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_PREFIX_PATH=$INSTALL_ORTOOLS/lib/cmake \
 	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache
 make
 make install
@@ -22,7 +37,7 @@ make install
 cd /data/src/OpenLane
 tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 tag=${tag//v/}
-tag=${tag//^*/}
+tag=${tag//\^*/}
 if [[ "$tag" == "undefined" ]]; then tag=$d; fi
 rm -rf /data/tools/OpenLane-$tag
 mkdir -p /data/tools/OpenLane-$tag/install
@@ -46,10 +61,26 @@ echo 'set ::env(VIRTUAL_ENV) "$OL_INSTALL_DIR/venv"'                   >> /data/
 cd /data/src/yosys
 tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 tag=${tag//v/}
-tag=${tag//^*/}
+tag=${tag//\^*/}
 tag=${tag//yosys-/}
 if [[ "$tag" == "undefined" ]]; then tag=$d; fi
 make config-gcc
 make PREFIX=/data/tools/yosys-$tag
 make PREFIX=/data/tools/yosys-$tag install
+
+# ngspice
+cd /data/src/ngspice
+tag=$(git name-rev --tags --name-only $(git rev-parse HEAD))
+tag=${tag//v/}
+tag=${tag//\^*/}
+if [[ "$tag" == "undefined" ]]; then tag=$d; fi
+rm -rf /data/tools/ngspice-$tag
+./autogen.sh --adms
+mkdir -p build && cd build
+../configure --prefix=/data/tools/ngspice-$tag --with-x --enable-xspice \
+	--enable-cider --with-readline=yes --enable-openmp --enable-adms \
+	--disable-debug
+#CFLAGS="-m64 -O2" LDFLAGS="-m64 -s"
+make
+make install
 
