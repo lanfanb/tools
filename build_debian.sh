27f7ce8627430355c3e9a487233d155d86ad3cec
Fan Lan <lanfanllrr@gmail.com>
2022-12-08 18:33:28 +0800

    add debian

--- build_debian.sh
+++ build_debian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for d in openeuler centos debian
+for d in debian
 do
 	echo "====> $d"
 	podman build --security-opt seccomp=unconfined --rm --no-cache -v /data:/data -v /root:/root \
-		--logfile=$d.log -t docker.io/lanfanb/siqbase:tools.$d -f Dockerfile.$d .
+		-t docker.io/lanfanb/siqbase:tools.$d -f Dockerfile.$d .
 	podman rmi docker.io/lanfanb/siqbase:tools.$d
 done
 
