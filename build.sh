ca84467feba97ba48f6e810ff5e6b65076ae0e2b
Fan Lan <lanfanllrr@gmail.com>
2022-12-14 13:40:11 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-for d in openeuler centos debian ubuntu
+os=$1
+
+if [[ "$os" == "all" ]]
+then
+	os_list="openeuler centos debian ubuntu"
+else
+	os_list=$os
+fi
+
+for d in $os_list
 do
 	echo "====> $d"
 	podman build --security-opt seccomp=unconfined --rm --no-cache -v /data:/data -v /root:/root \
