74019d9cef61ef0f1d8b618d9c6de664ec5d7435
Fan Lan <lanfanllrr@gmail.com>
2023-01-04 10:20:11 +0800

    add swap for rpi web

--- exec_rpi_web.sh
+++ exec_rpi_web.sh
@@ -8,6 +8,7 @@ export d=$(date +'%Y.%m.%d')
 # WebKit
 cd /data/WebKit
 tag=$d
+#sudo swapon /data/file8GB.swap
 export http_proxy=http://127.0.0.1:1081
 export https_proxy=http://127.0.0.1:1081
 export WEBKIT_JHBUILD=1
@@ -16,4 +17,5 @@ export PATH=$PATH:/usr/lib/aarch64-linux-gnu/gdk-pixbuf-2.0
 Tools/Scripts/update-webkitgtk-libs
 #Tools/Scripts/build-webkit --gtk
 #Tools/Scripts/run-minibrowser --gtk
+#sudo swapoff /data/file8GB.swap
 
